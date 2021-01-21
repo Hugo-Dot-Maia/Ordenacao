88cedbec059f0a0c76a1c9d14883882d05c98408
Hugo Guimarães Maia <hugoguima75@outlook.com>
2021-01-21 20:35:29 -0300

    Classe Base de herança adicionada

---
+++ Base.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Base
+{
+	private int[] numerosAleatorios = new int[5];
+
+	public setNumeros()
+    {
+		for (int i = 0; i < numerosAleatorios.Length; i++)
+		{
+			numerosAleatorios[i] = randNum.Next(Min, Max);
+		}
+	}
+
+	public getNumeros()
+    {
+		return numerosAleatorios;
+    }
+
+
+	
+}
